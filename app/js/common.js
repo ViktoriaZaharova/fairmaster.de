b54eeb73e93798b3f8e72d66dee641ef379e6689
Viktoria <zaharova181114@gmail.com>
2022-01-25 21:15:22 +0200

    add pages

--- app/js/common.js
+++ app/js/common.js
@@ -241,3 +241,37 @@ $(document).ready(function () {
 
 });
 
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation')
+
+        // Loop over them and prevent submission
+        Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+    }, false)
+}());
+
+$('#dt-basic-checkbox').dataTable({
+
+    columnDefs: [{
+        orderable: false,
+        className: 'select-checkbox',
+        targets: 0
+    }],
+    select: {
+        style: 'os',
+        selector: 'td:first-child'
+    }
+});
